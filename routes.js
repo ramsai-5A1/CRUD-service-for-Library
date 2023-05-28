f86889bbc9bb6200af26a128ac792038212a3a1d
Ramsai <ramsaicrony@gmail.com>
2023-05-28 22:38:01 +0530

    Cleaned the code a bit by adding FIND method and by removing FOR loop from code

--- routes.js
+++ routes.js
@@ -14,9 +14,6 @@ router.post("/addBook", (req, res) => {
 
 //Reading all the records from Database
 router.get("/books", (req, res) => {
-    console.log("Printing from Database");
-    console.log(dataBase);
-
     if(dataBase.length == 0) {
         res.status(201).send("No Books Present within the Library");
     }
@@ -26,7 +23,7 @@ router.get("/books", (req, res) => {
 //Updating a single record from Database based on ID
 router.get("/book/:id", (req, res) => {
     let userId = req.params.id;
-    let book = dataBase.find((book) => book.id == userId);
+    let book = dataBase.find((book) => book.id === userId);
     if(!book) {
         res.status(404).json({message: "Book Not Found"});
     }
@@ -36,17 +33,16 @@ router.get("/book/:id", (req, res) => {
 //Deleting the book from Database
 router.delete("/book/:id", (req, res) => {
     const userId = req.params.id;
-    for(let index = 0; index < dataBase.length; index++) {
-        let currObject = dataBase[index];
-        if(currObject.id == userId) {
-            let tempDatabase = dataBase.slice(0, index);
-            tempDatabase = [...tempDatabase, ...dataBase.slice(index + 1, dataBase.length)];
-        
-            dataBase = tempDatabase;
-            res.status(201).send("Successfully Deleted");
-        }
+    const index = dataBase.findIndex((book) => book.id === userId);
+    if(index == -1) {
+        res.status(404).send("Book Not Found");
     }
-    res.status(404).send("Book Not Found");
+
+    let tempDatabase = dataBase.slice(0, index);
+    tempDatabase = [...tempDatabase, ...dataBase.slice(index + 1, dataBase.length)];
+
+    dataBase = [...tempDatabase];
+    res.status(201).send("Successfully Deleted");
 });
 
 module.exports = router;
